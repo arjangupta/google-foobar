4ca461864cb28525f46340f442c79be05d5c784a
Arjan Gupta <arjangupta95@gmail.com>
2020-04-16 01:20:05 -0500

    Add WIP solution for first challenge

--- braille-translation/solution.py
+++ braille-translation/solution.py
@@ -1,2 +1,15 @@
 def solution(s):
-    # Your code here
+    # Parameter s is the plain text word
+    
+    output_str = ""
+    
+    if type(s) is not str:
+        return output_str
+    
+    for char in str:
+        if char == ' ':
+            output_str += "000000"
+        elif char.isUpper():
+            output_str += "000001"
+        else:
+            
