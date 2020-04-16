91d948ee871a0163955588d16109e65351cc5651
Arjan Gupta <arjangupta95@gmail.com>
2020-04-16 15:55:12 -0500

    Print out separated character and non-char encodings

--- braille-translation/reverse_engineer.py
+++ braille-translation/reverse_engineer.py
@@ -1,36 +1,46 @@
-def reverse_engineer(text_str, input_str):
+def reverse_engineer(text_str, binary_str):
     # Check input str
-    for char in input_str:
+    for char in binary_str:
         if char != '0' and char != '1':
             print("Something's wrong with your damn input.")
             return
     
+    print ""
     print "--------- TEST CASE ---------"
     print "The given text string is: " + text_str
     print "Length of the text string is", len(text_str)
-    print "Length of the binary string is", len(input_str)
-    print "The binary string length / 6 is", ( len(input_str) / 6)
+    print "Length of the binary string is", len(binary_str)
+    print "The binary string length / 6 is", ( len(binary_str) / 6)
     
-    if ( len(input_str) / 6 ) < len(text_str):
+    # Ensure binary_str vs text_str makes sense
+    if ( len(binary_str) / 6 ) < len(text_str):
         print("Error: the input string length / 6 is less than the text string length.")
         return
     
-    print("Now printing the corresponding char encodings:")
+    print "Now writing the corresponding char encodings to file"
 
     encodings_dict = {}
     i = 0
     for char in text_str:
         if char.isupper():
-            encodings_dict["cap_char"] = input_str[i:(i+6)]
+            encodings_dict["cap_char"] = binary_str[i:(i+6)]
             i += 6
 
         if char == ' ':
-            encodings_dict["space_char"] = input_str[i:(i+6)]
+            encodings_dict["space_char"] = binary_str[i:(i+6)]
         else:
-            encodings_dict[char.lower()] = input_str[i:(i+6)]
+            encodings_dict[char.lower()] = binary_str[i:(i+6)]
         i += 6
     
+    non_letter_encoding = {}
     for key in sorted(encodings_dict.keys()):
+        if key != "space_char" and key != "cap_char":
+            print key + ": " + encodings_dict[key]
+        else:
+            non_letter_encoding[key] = encodings_dict[key]
+    
+    print ""
+    for key in non_letter_encoding:
         print key + ": " + encodings_dict[key]
 
 def main():
