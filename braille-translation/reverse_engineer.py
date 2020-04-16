7d0a36d8e22b4ffc229ed27181cbb09b3bd51cd9
Arjan Gupta <arjangupta95@gmail.com>
2020-04-16 03:14:10 -0500

    Attempt to read all test cases from input file

--- braille-translation/reverse_engineer.py
+++ braille-translation/reverse_engineer.py
@@ -1,20 +1,17 @@
-# import sys
+def reverse_engineer(input_str, text_str):
 
-def main():
-    input_str = input()
-
+    # Check input str
     for char in input_str:
         if char != '0' and char != '1':
             print("Something's wrong with your damn input.")
             return
     
-    text_str = "The quick brown fox jumps over the lazy dog"
-    print('\nI assume this means "' + text_str + '"')
+    print("The given text string is: " + text_str)
     print("Length of the text string is", len(text_str))
     print("Length of the input string is", len(input_str))
     print("The input string length / 6 is", ( len(input_str) / 6) )
     
-    if ( len(input_str) / 6) < len(text_str):
+    if ( len(input_str) / 6 ) < len(text_str):
         print("Error: the input string length / 6 is less than the text string length.")
         return
     
@@ -27,6 +24,17 @@ def main():
             i += 6
         print(char.lower() + ': '  + input_str[i:(i+6)])
         i += 6
+
+def main():
+    text_str_arr = []
+    input_str_arr = []
+    
+    for i in range(3):
+        text_str[i] = input()
+        input_str[i] = input()
+    
+    for i in range(3):
+        reverse_engineer(text_str[i], input_str[i])
 
 if __name__ == "__main__":
     main()
