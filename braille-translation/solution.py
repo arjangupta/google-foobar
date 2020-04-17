ee629ae209888d4597fb6dee0ae75acbff6d67bc
Arjan Gupta <arjangupta95@gmail.com>
2020-04-16 19:06:31 -0500

    Give some finishing touches to the solution

--- braille-translation/solution.py
+++ braille-translation/solution.py
@@ -16,14 +16,14 @@ def braille_encode(char):
 
     # Known data for encoding
     capital_char_prefix = "000001"
-    space_char_prefix   = "000000"
+    space_char_encoding = "000000"
     letter_arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+    first10_letter_decimal_values = [32, 48, 36, 38, 34, 52, 54, 50, 20, 22]
     w_encoding = "010111" # w is an exception to the pattern. Binary string is found by: ( '0' + bin(22 + 1)[2:] )
-    first10_letter_values = [32, 48, 36, 38, 34, 52, 54, 50, 20, 22]
 
     # Begin building encoded char string
     if char == ' ':
-        return space_char_prefix
+        return space_char_encoding
 
     encoded_str = ""
     if char.isupper():
@@ -35,10 +35,10 @@ def braille_encode(char):
         return encoded_str
 
     # Find which row and column the character belongs to
-    letter_column = letter_arr.index(char) % len(first10_letter_values)  
-    letter_row    = letter_arr.index(char) / len(first10_letter_values)
+    letter_column = letter_arr.index(char) % len(first10_letter_decimal_values)  
+    letter_row    = letter_arr.index(char) / len(first10_letter_decimal_values)
     # The 3rd dot is raised if it's in the second row, and the 3rd and 6th dot is raised in the third row
-    letter_decimal_value = first10_letter_values[letter_column]
+    letter_decimal_value = first10_letter_decimal_values[letter_column]
     if letter_row == 1:
         letter_decimal_value += 8
     elif letter_row == 2:
