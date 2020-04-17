6c64848e87b950c4d3b81e6f8404a8fe51aaafff
Arjan Gupta <arjangupta95@gmail.com>
2020-04-16 21:16:26 -0500

    Add empty solution for LAMBs

--- lovely-lucky-lambs/solution.py
+++ lovely-lucky-lambs/solution.py
@@ -1,2 +1,19 @@
-def​ ​solution(total_lambs):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+def solution(total_lambs):
+    # Code
+    return
+
+def main():
+    result = solution(10)
+    if result == 1:
+        print "Test case passed for 10 LAMBs"
+    else:
+        print "Test case failed for 10 LAMBs"
+
+    result = solution(143)
+    if result == 3:
+        print "Test case passed for 143 LAMBs"
+    else:
+        print "Test case failed for 143 LAMBs"
+
+if __name__ == "__main__":
+    main()
