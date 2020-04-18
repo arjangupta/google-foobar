cccb5fc79fd51e01419d352ce3086be589d4d0ae
Arjan Gupta <arjangupta95@gmail.com>
2020-04-18 13:39:52 -0500

    Write the test cases for the solution

--- lovely-lucky-lambs/solution.py
+++ lovely-lucky-lambs/solution.py
@@ -1,19 +1,25 @@
 def solution(total_lambs):
-    # Code
-    return
+    return 0
+
+def test_case(total_lambs, computed_result, expected_result):
+    if computed_result == expected_result:
+        print "Test case passed for", total_lambs, "LAMBs"
+    else:
+        print "Test case failed for", total_lambs, "LAMBs"
+
 
 def main():
-    result = solution(10)
-    if result == 1:
-        print "Test case passed for 10 LAMBs"
-    else:
-        print "Test case failed for 10 LAMBs"
+    total_lambs = 10
+    result      = solution(total_lambs)
+    test_case(total_lambs, result, 1)
+    
+    total_lambs = 143
+    result      = solution(total_lambs)
+    test_case(total_lambs, result, 3)
 
-    result = solution(143)
-    if result == 3:
-        print "Test case passed for 143 LAMBs"
-    else:
-        print "Test case failed for 143 LAMBs"
+    total_lambs = 223
+    result      = solution(total_lambs)
+    test_case(total_lambs, result, 2)
 
 if __name__ == "__main__":
     main()
