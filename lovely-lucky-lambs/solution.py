8f4151ab0be588cdacd0ede47dba61cc40306baf
Arjan Gupta <arjangupta95@gmail.com>
2020-04-18 15:40:00 -0500

    Write the algorithm for distributing total_lambs in the generous way

--- lovely-lucky-lambs/solution.py
+++ lovely-lucky-lambs/solution.py
@@ -1,11 +1,12 @@
 def solution(total_lambs):
-    print ""
-    print "Distributing", total_lambs, "LAMBs"
+    print "" # TODO: delete line
+    print "Distributing", total_lambs, "LAMBs" # TODO: delete line
     # Get number of henchman you can give a stingy distribution to
-    num_henchman_stingy = calculate_stingy_distribution(total_lambs)
-    
-    return 0
-
+    num_henchmen_stingy_distribution   = calculate_stingy_distribution(total_lambs)
+    # Get number of henchmen you can give a generous distribution to
+    num_henchmen_generous_distribution = calculate_generous_distribution(total_lambs)   
+    # Return the difference
+    return (num_henchmen_stingy_distribution - num_henchmen_generous_distribution)
 
 def calculate_stingy_distribution(total_lambs):
     # Traverse the stingy linked list
@@ -24,8 +25,36 @@ def calculate_stingy_distribution(total_lambs):
         # Calculate the next amount
         lamb_amount_for_next_henchman = first_subordinate_lambs + second_subordinate_lambs
 
-    print "STINGY: A total of", lambs_handed_out_so_far, "LAMBs were handed out among", number_of_henchmen, "henchmen"
+    print "STINGY: A total of", lambs_handed_out_so_far, "LAMBs were handed out among", number_of_henchmen, "henchmen" # TODO: delete line
 
+    return number_of_henchmen
+
+def calculate_generous_distribution(total_lambs):
+    # Traverse the generous linked list
+    lambs_handed_out_so_far = 0
+    number_of_henchmen      = 0
+    first_subordinate_lambs  = 0
+    second_subordinate_lambs = 0
+    lamb_amount_for_next_henchman = 1
+    while lambs_handed_out_so_far + lamb_amount_for_next_henchman <= total_lambs:
+        # Hand out a lamb
+        number_of_henchmen += 1
+        lambs_handed_out_so_far += lamb_amount_for_next_henchman
+        # Update the subordinate amounts
+        second_subordinate_lambs = first_subordinate_lambs
+        first_subordinate_lambs  = lamb_amount_for_next_henchman
+        # Calculate the next amount
+        lamb_amount_for_next_henchman = lamb_amount_for_next_henchman * 2
+    else:
+        # Try to give the next henchman the sum of the 2 subordinates
+        lamb_amount_for_next_henchman = first_subordinate_lambs + second_subordinate_lambs
+        if lambs_handed_out_so_far + lamb_amount_for_next_henchman <= total_lambs:
+            # Hand out a lamb
+            number_of_henchmen += 1
+            lambs_handed_out_so_far += lamb_amount_for_next_henchman # TODO: delete line
+    
+    print "GENEROUS: A total of", lambs_handed_out_so_far, "LAMBs were handed out among", number_of_henchmen, "henchmen" # TODO: delete line
+    
     return number_of_henchmen
 
 def test_case(total_lambs, computed_result, expected_result):
