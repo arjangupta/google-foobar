6faba66b353a093196909216c896fb86a2b33840
Arjan Gupta <arjangupta95@gmail.com>
2020-04-19 01:25:55 -0500

    Revert "Simplify the while-else block for generous distribution"
    This reverts commit 16b7e553e01d84774e21c4e358d81ed184bf1377.

--- lovely-lucky-lambs/solution.py
+++ lovely-lucky-lambs/solution.py
@@ -39,8 +39,21 @@ def calculate_generous_distribution(total_lambs):
     else:
         first_subordinate_lambs  = lamb_amount_for_next_henchman / 2
         second_subordinate_lambs = first_subordinate_lambs / 2
-        number_of_henchmen += calculate_stingy_distribution(total_lambs, lambs_handed_out_so_far, 
-                                               first_subordinate_lambs, second_subordinate_lambs)
+        # Try to give the next henchman the sum of the 2 subordinates
+        lamb_amount_for_next_henchman = first_subordinate_lambs + second_subordinate_lambs
+        if lambs_handed_out_so_far + lamb_amount_for_next_henchman <= total_lambs:
+            # Hand out a lamb
+            number_of_henchmen += 1
+            lambs_handed_out_so_far += lamb_amount_for_next_henchman
+        else:
+            temp_lambs_so_far = lambs_handed_out_so_far - first_subordinate_lambs
+            temp_num_henchmen = number_of_henchmen - 1
+            second_subordinate_lambs /= 2
+            first_subordinate_lambs  /= 2
+            temp_num_henchmen += calculate_stingy_distribution(total_lambs, temp_lambs_so_far, 
+                                                   first_subordinate_lambs, second_subordinate_lambs)
+            if number_of_henchmen < temp_num_henchmen:
+                number_of_henchmen = temp_num_henchmen
     return number_of_henchmen
 
 def test_case(total_lambs, computed_result, expected_result):
@@ -68,7 +81,7 @@ def main():
 
     total_lambs = 383
     result      = solution(total_lambs)
-    test_case(total_lambs, result, 4)
+    test_case(total_lambs, result, 3)
 
 if __name__ == "__main__":
     main()
