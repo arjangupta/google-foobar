5b2be17c24f64b4d33995ae134f7ec6cf103562e
Arjan Gupta <arjangupta95@gmail.com>
2020-04-27 01:32:19 -0500

    Write the basics of the sublisting algorithm

--- ion-flux-relabeling/solution.py
+++ ion-flux-relabeling/solution.py
@@ -1,5 +1,46 @@
+# Returns the array of parents
+# h is the height of the tree
+# q is the list of elements in tree for which 
+# parents are to be found
 def solution(h, q):
-    return []
+    p = [find_parent(h, i) for i in q]
+    print p # TODO: remove
+    return p
+
+# Returns a parent for a given node of the tree
+def find_parent(height, element):
+    absolute_parent = (2**height - 1)
+    if element >= absolute_parent:
+        return -1
+    
+    # Begin the sublisting algorithm.
+    
+    # Rules of the binary tree:
+    # 1. It is a perfect binary tree
+    # 2. The post order traversal list is an ascending order
+    #    integer list from 1 to (2**h - 1)
+    whole_tree       = range(1, absolute_parent + 1)
+    current_sublist  = whole_tree
+    current_parent   = -1
+    current_height   = height
+    found_parent     = 0
+    left_sublist     = []
+    right_sublist    = []
+    while found_parent == 0:
+        print "current list is", current_sublist
+        current_parent  = current_sublist[-1]
+        current_height -= 1
+        left_sublist    = current_sublist[ : (2**current_height - 1) ]
+        right_sublist   = current_sublist[ (2**current_height - 1) : -1 ]
+        print "Left sublist is", left_sublist
+        print "Right sublist is", right_sublist
+        if element == left_sublist[-1] or element == right_sublist[-1]:
+            found_parent == current_parent
+        elif element in left_sublist:
+            current_sublist = left_sublist
+        else:
+            current_sublist = right_sublist
+    return found_parent
 
 def test_case(h, q, p):
     if (p != solution(h, q)):
@@ -7,7 +48,7 @@ def test_case(h, q, p):
 
 def main():
     # Run test cases
-    test_case(3, [7, 3, 5, 1], [-1,7,6,3])
+    test_case(3, [7, 3, 5, 1], [-1, 7, 6, 3])
     test_case(5, [19, 14, 28], [21, 15, 29])
     test_case(4, [14, 4, 8, 20, 12], [15, 6, 10, -1, 13])
 
