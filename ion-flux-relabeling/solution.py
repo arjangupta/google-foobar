f3335e4906189a0fe735e4fc43e2183b14f866f3
Arjan Gupta <arjangupta95@gmail.com>
2020-04-27 00:08:00 -0500

    Set up some basic test cases

--- ion-flux-relabeling/solution.py
+++ ion-flux-relabeling/solution.py
@@ -1,2 +1,15 @@
 def solution(h, q):
-    # Your code here
+    return []
+
+def test_case(h, q, p):
+    if (p != solution(h, q)):
+        print "Test case for", q, "failed."
+
+def main():
+    # Run test cases
+    test_case(3, [7, 3, 5, 1], [-1,7,6,3])
+    test_case(5, [19, 14, 28], [21, 15, 29])
+    test_case(4, [14, 4, 8, 20, 12], [15, 6, 10, -1, 13])
+
+if __name__ == "__main__":
+    main()
