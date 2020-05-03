79b322f856176edb2187266bd427de40654f6c3b
Arjan Gupta <arjangupta95@gmail.com>
2020-05-03 18:13:00 -0500

    Add bounds checking for employee ID 2 billion, even though it didn't affect google's test cases

--- queue-to-do/solution.py
+++ queue-to-do/solution.py
@@ -2,11 +2,17 @@ def solution(start, length):
     partition_position  = length
     current_xor_result  = 0
     current_employee_id = start
-    while partition_position > 0:
+    break_loops = False
+    while (partition_position > 0) and (not break_loops):
         for employee_position in range(length):
             if employee_position < partition_position:
                 current_xor_result ^= current_employee_id
             current_employee_id += 1
+            if current_employee_id > 2*10**9:
+                break_loops = True
+                break
+        if break_loops:
+            break
         partition_position -= 1
     return current_xor_result
 
@@ -21,6 +27,9 @@ def main():
     # Run the test cases
     test_case(0, 3, 2)
     test_case(17, 4, 14)
+    test_case(1, 1, 1)
+    test_case((2*10**9), 2, (2*10**9))
+    test_case((2*10**9), 10, (2*10**9))
 
 if __name__ == "__main__":
     main()
