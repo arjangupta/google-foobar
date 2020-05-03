cb7c694765f8de6efe33840752170eebcb2f6643
Arjan Gupta <arjangupta95@gmail.com>
2020-05-03 14:10:58 -0500

    Set up the basic tests for queue-to-do/

--- queue-to-do/solution.py
+++ queue-to-do/solution.py
@@ -1,2 +1,17 @@
-def​ ​solution(start,​ ​length):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+def solution(start, length):
+    return 100
+
+def test_case(start, length, expected_result):
+    print "Test for ( " + str(start) + ", " + str(length) + " ) ",
+    if expected_result != solution(start, length):
+        print "FAILED."
+    else:
+        print "passed."
+
+def main():
+    # Run the test cases
+    test_case(0, 3, 2)
+    test_case(17, 4, 14)
+
+if __name__ == "__main__":
+    main()
