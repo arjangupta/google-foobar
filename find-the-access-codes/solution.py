81ecd2e74cb41a966a40fadbbc4e913791f3e612
Arjan Gupta <arjangupta95@gmail.com>
2020-05-31 15:19:30 -0500

    Set up the given test cases for find-the-access-codes

--- find-the-access-codes/solution.py
+++ find-the-access-codes/solution.py
@@ -1,2 +1,16 @@
 def solution(l):
-    # Your code here
+    return 0
+
+def test_case(l, expected_result):
+    print "Test for", l,
+    if expected_result != solution(l):
+        print "FAILED."
+    else:
+        print "passed."
+
+def main():
+    test_case([1, 2, 3, 4, 5, 6], 3)
+    test_case([1, 1, 1], 1)
+
+if __name__ == "__main__":
+    main()
