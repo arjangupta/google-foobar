e9a5025509ec864ed6619d52da24420c0a814ebb
Arjan Gupta <arjangupta95@gmail.com>
2020-06-01 18:15:38 -0500

    Rewrite the algorithm to use a "partition"

--- find-the-access-codes/solution.py
+++ find-the-access-codes/solution.py
@@ -1,23 +1,18 @@
 def solution(l):
-    lucky_pairs_spread = [0] * len(l)
     lucky_triplets = []
     for i in range(len(l)):
         pairs = []
         first_sublist = l[:i]
         for j in range(len(first_sublist)):
             if l[i] % first_sublist[j] == 0:
-                lucky_pairs_spread[i] += 1
                 pair = [first_sublist[j], l[i]]
                 pairs.append(pair)
         second_sublist = l[i+1:]
         for k in range(len(second_sublist)):
             if second_sublist[k] % l[i] == 0:
                 for pair in pairs:
-                    print "pair", pair
-                    triplet = pair
-                    triplet.append(second_sublist[k])
+                    triplet = [pair[0], pair[1], second_sublist[k]] 
                     if triplet not in lucky_triplets:
-                        print triplet, "final appendable"
                         lucky_triplets.append(triplet)
     return len(lucky_triplets)
 
