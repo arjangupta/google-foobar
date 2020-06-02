b03bbd0711c3adcac2c76e9d477a76a5881b8dec
Arjan Gupta <arjangupta95@gmail.com>
2020-06-02 13:51:25 -0500

    Implement the fastest solution

--- find-the-access-codes/solution.py
+++ find-the-access-codes/solution.py
@@ -1,20 +1,15 @@
 def solution(l):
-    lucky_triplets = []
+    triples = 0
+    pairs = [0] * len(l)
     for i in range(len(l)):
-        pairs = []
-        first_sublist = l[:i]
-        for j in range(len(first_sublist)):
-            if l[i] % first_sublist[j] == 0:
-                pair = [first_sublist[j], l[i]]
-                pairs.append(pair)
-        second_sublist = l[i+1:]
-        for k in range(len(second_sublist)):
-            if second_sublist[k] % l[i] == 0:
-                for pair in pairs:
-                    triplet = [pair[0], pair[1], second_sublist[k]] 
-                    if triplet not in lucky_triplets:
-                        lucky_triplets.append(triplet)
-    return len(lucky_triplets)
+        for j in range(i):
+            if l[i] % l[j] == 0:
+                pairs[i] += 1
+    for i in range(len(l)):
+        for j in range(i):
+            if l[i] % l[j] == 0:
+                triples += pairs[j]
+    return triples
 
 def test_case(l, expected_result):
     print "Test for", l,
@@ -27,6 +22,7 @@ def test_case(l, expected_result):
 def main():
     test_case([1, 2, 3, 4, 5, 6], 3)
     test_case([1, 1, 1], 1)
+    test_case([1, 1, 1, 1], 4)
 
 if __name__ == "__main__":
     main()
